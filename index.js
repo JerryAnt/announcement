ea2a6c800b0e71fc0966d3a5ed5775ba62bebb70
Jeremy Antoine <jeremincanada@gmail.com>
2021-11-12 13:31:29 -0500

    cleaned up the index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // I want my page to show a counterclock with remaining number of days, hours, minutes, seconds
 
-// call all the variables that are going to be used
+// Call all the variables that are going to be used for instance
 const endTime = new Date("April 1, 2022 13:05:00 GMT+00:00").getTime();
 const timer = document.getElementById('timer');
 const th1 = document.getElementById('timer-hours1');
@@ -10,20 +10,21 @@ const tm2 = document.getElementById('timer-minutes2');
 const ts1 = document.getElementById('timer-seconds1');
 const ts2 = document.getElementById('timer-seconds2');
 
-// countDownTimer is my variable to count down the time every seconds
+// CountDownTimer is my variable to count down the time every seconds
 const countDownTimer = setInterval(function (){
 const currentTime = new Date().getTime();
 const timeLeft = endTime - currentTime;
-// count the days (biggest period of time) over timeLeft
+
+// Count the days (biggest period of time) over timeLeft
 const days = Math.floor(timeLeft / (1000*60*60*24));
-// count the hours with the remainder after the days equation is converted
+// Count the hours with the remainder after the days equation is converted
 const hours = Math.floor((timeLeft % (1000*60*60*24))/(1000*60*60));
-// count the minutes with the remainder after the days equation is converted
+// Count the minutes with the remainder after the days equation is converted
 const minutes = Math.floor((timeLeft % (1000*60*60))/(1000*60));
-// count the seconds with the remainder after the days equation is converted
+// Count the seconds with the remainder after the days equation is converted
 const seconds = Math.floor((timeLeft % (1000*60))/1000);
 
-// not to encounter an undefined message when the number is 1 digit
+// Avoid undefined message when the number is made of 1 digit
 const noUndefined = (number, minIntegerDigits) => {
     return number.toLocaleString(undefined, { minimumIntegerDigits: minIntegerDigits });
   };
@@ -37,7 +38,10 @@ const minutes2 = noUndefined(minutes, 2).toString().split("")[1];
 const seconds1 = noUndefined(seconds, 2).toString().split("")[0];
 const seconds2 = noUndefined(seconds, 2).toString().split("")[1];
 
+// Display the counter as one line
 timer.innerHTML = days + " days, "  + hours1 + hours2 + " hours, " + minutes1 + minutes2 + " minutes and " + seconds1 + seconds2 + " seconds";
+
+// Display the counter as different elements to work the css through
 th1.innerHTML = hours1;
 th2.innerHTML = hours2;
 tm1.innerHTML = minutes1;
